4a8a198d40ff50cd13f64832f545d7e6ffe36090
alanperez <alanp9six@gmail.com>
2019-07-26 12:41:27 -0500

    :sparkles: Set up the file to render smurfs.. implemented state and effect hook also redux

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,47 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+import axios from 'axios'
+
+import { connect } from 'react-redux'
+import Smurfs from './Smurfs'
+import {getSmurf} from '../redux/actions/smurfAction'
+const App = (props) =>  {
+  
+
+ const[data,setData] = useState({smurfs: []})
+  useEffect( () => {
+    axios (
+      props.getSmurf()
+    )
+    setData(props.getSmurf())
+    // console.log(smurf.data)
+    // console.log('clg smurf in effect: ', setData)
+
+
+  }, [props.getSmurf])
+// const getSmurfs = e => {
+//   e.preventDefault()
+//   props.getSmurf()
+// }
+
+  
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        
+        <h4>{props.smurfs.map(smurf => {
+          return <Smurfs smurf={smurf} />
+        })}</h4>
+         
       </div>
     );
-  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
+export default connect(mapStateToProps, {getSmurf})(App);
